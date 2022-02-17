e75bb27b1a3588ed57b47accb2295e972981de43
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-17 09:19:46 -0300

    Update view, form and url mapping
    - Removed generic view
    - Added Django Form to render Model form fields
    - Updated view logic accordingly to Django Form fields

---
+++ register/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from .models import ObjectiveKeyResult
+from django.utils.translation import gettext_lazy as _
+
+class OkrForm(ModelForm):
+    class Meta:
+        model = ObjectiveKeyResult
+        fields = ['integrante','objetivo','resultado_1','resultado_2','resultado_3','resultado_4','resultado_5',]
+        labels = {
+            'integrante': 'Nome do Integrante',
+        }
--- register/views.py
+++ register/views.py
@@ -1,11 +1,34 @@
+import json
 from django.shortcuts import render
-from django.views import generic
-from .models import Integrante
+from django.http import HttpResponseRedirect
+from .models import Integrante, ObjectiveKeyResult
+
+from .forms import OkrForm
 
 # Create your views here.
-class IndexView(generic.ListView):
-    template_name = 'register/form.html'
-    context_object_name = "integrante_list"
+def index(request):
+    if request.method == 'POST':
+        form = OkrForm(request.POST)
+        if form.is_valid():
+            okr = ObjectiveKeyResult()
+            okr.integrante = form.cleaned_data['integrante']
+            okr.objetivo = form.cleaned_data['objetivo']
+            okr.resultado_1 = form.cleaned_data['resultado_1']
+            okr.resultado_2 = form.cleaned_data['resultado_2']
+            okr.resultado_3 = form.cleaned_data['resultado_3']
+            okr.resultado_4 = form.cleaned_data['resultado_4']
+            okr.resultado_5 = form.cleaned_data['resultado_5']
+            
+            print(form.cleaned_data['integrante'])
+            print(form.cleaned_data['objetivo'])
+            print(form.cleaned_data['resultado_1'])
+            print(form.cleaned_data['resultado_2'])
+            print(form.cleaned_data['resultado_3'])
+            print(form.cleaned_data['resultado_4'])
+            print(form.cleaned_data['resultado_5'])
+            form.save()
+            return HttpResponseRedirect('/register/')
+    else:
+        form = OkrForm()
 
-    def get_queryset(self):
-        return Integrante.objects.all()
+    return render(request, 'register/form.html', {'form': form})
