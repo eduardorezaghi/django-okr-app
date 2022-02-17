ea9cbc1a19295c36991199fe74aa261181b9a11c
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-17 09:17:03 -0300

    Update OKR model fields
    - Blank=true setted to make the field optional

--- register/models.py
+++ register/models.py
@@ -12,10 +12,10 @@ class ObjectiveKeyResult(models.Model):
     integrante = models.ForeignKey(Integrante, on_delete=models.CASCADE)
     objetivo = models.CharField(max_length=200)
     resultado_1 = models.CharField(max_length=300, null=False)
-    resultado_2 = models.CharField(max_length=300, null=False)
-    resultado_3 = models.CharField(max_length=300, null=False)
-    resultado_4 = models.CharField(max_length=300, null=False)
-    resultado_5 = models.CharField(max_length=300, null=False)
+    resultado_2 = models.CharField(max_length=300, null=False, blank=True)
+    resultado_3 = models.CharField(max_length=300, null=False, blank=True)
+    resultado_4 = models.CharField(max_length=300, null=False, blank=True)
+    resultado_5 = models.CharField(max_length=300, null=False, blank=True)
 
     def __str__(self):
         return self.objetivo
