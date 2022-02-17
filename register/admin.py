22beed55dc08b421fad770bd28989049da975701
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-17 09:17:57 -0300

    Update Django Admin page
    - Add OKR mapping directly to Integrante Admin page

--- register/admin.py
+++ register/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Integrante, ObjectiveKeyResult
 
 # Register your models here.
-admin.site.register(Integrante)
-admin.site.register(ObjectiveKeyResult)
+class ObjectiveKeyResultInLine(admin.StackedInline):
+    model = ObjectiveKeyResult
+    extra = 1
+
+class IntegranteAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Nome',          {"fields": ('nome',),}),
+    )
+    inlines = [ObjectiveKeyResultInLine]
+
+admin.site.register(Integrante, IntegranteAdmin)
