0a037479043fe838f8baf8c11a36c651a49b78de
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-18 15:44:00 -0300

    Modify index view
    - Removed all form.cleaned_data attributes lookup from POST for simplicity reasons. It was unneeded code, as Django ModelForm already saves all cleaned data using form.save() method.

--- register/views.py
+++ register/views.py
@@ -1,6 +1,7 @@
 import json
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Integrante, ObjectiveKeyResult
 
 from .forms import OkrForm
@@ -10,23 +11,9 @@ def index(request):
     if request.method == 'POST':
         form = OkrForm(request.POST)
         if form.is_valid():
-            okr = ObjectiveKeyResult()
-            okr.integrante = form.cleaned_data['integrante']
-            okr.objetivo = form.cleaned_data['objetivo']
-            okr.resultado_1 = form.cleaned_data['resultado_1']
-            okr.resultado_2 = form.cleaned_data['resultado_2']
-            okr.resultado_3 = form.cleaned_data['resultado_3']
-            okr.resultado_4 = form.cleaned_data['resultado_4']
-            okr.resultado_5 = form.cleaned_data['resultado_5']
-            
-            print(form.cleaned_data['integrante'])
-            print(form.cleaned_data['objetivo'])
-            print(form.cleaned_data['resultado_1'])
-            print(form.cleaned_data['resultado_2'])
-            print(form.cleaned_data['resultado_3'])
-            print(form.cleaned_data['resultado_4'])
-            print(form.cleaned_data['resultado_5'])
             form.save()
+            intg = form.cleaned_data.get('integrante')
+            messages.success(request, f"OKR criada para o integrante {intg}")
             return HttpResponseRedirect('/register/')
     else:
         form = OkrForm()
