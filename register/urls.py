3d61b9c98f151673ad1a812888e2de2a25d0a37a
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-24 09:46:08 -0300

    Update Project related files

--- register/urls.py
+++ register/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
+from atexit import register
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import OkrViewSet
 
 app_name = 'register'
+# Criando um roteador para realizar a configuração das URLS.
+router = DefaultRouter()
+
+router.register(r'okr-api', OkrViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('okr-api/', views.OkrList.as_view())
+    path('', include(router.urls)),
+    # path('okr-api/', views.OkrViewSet.as_view())
 ]
