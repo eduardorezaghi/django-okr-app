bf8cb7b19ea9e8e536916ecf1587d098dda7b993
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-26 18:20:27 -0300

    Remove UserSerializer and change custom field
    - Changed integrante field from CharField to StringRelatedField, read only

--- register/serializers.py
+++ register/serializers.py
@@ -1,7 +1,6 @@
 """
 Serializers for Models module.
 """
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Integrante, ObjectiveKeyResult
 
@@ -24,23 +23,11 @@ class ObjectiveKeyResultSerializer(serializers.ModelSerializer):
     """
     Serializer Class for ObjectiveKeyResult Model.
     """
-    integrante = serializers.CharField(source='integrante.nome')
+    integrante_nome = serializers.StringRelatedField(source='integrante.nome', read_only=True)
     class Meta:
         """
         Meta-information class for OKR Model Serializer
         """
         model = ObjectiveKeyResult
-        fields = ['id', 'integrante', 'objetivo', 'resultado_1',
-                  'resultado_2', 'resultado_3', 'resultado_4', 'resultado_5']
-
-
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializing an User Model from Django default auth system.
-    """
-    class Meta:
-        """
-        Meta-information class for User Model Serializer
-        """
-        model = User
-        fields = ['id', 'username', 'okrs']
+        fields = ['id', 'integrante', 'integrante_nome', 'objetivo', 'resultado_1',
+        'resultado_2', 'resultado_3', 'resultado_4', 'resultado_5']
