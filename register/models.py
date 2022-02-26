167df517bfbb2bd7e4fc82b8c290e6c5c4e2315c
Eduardo Rezaghi <58532229+eduardorezaghi@users.noreply.github.com>
2022-02-26 18:21:20 -0300

    Add model text validator
    - Validating input text with regexp, allowing only alphanumeric characters

--- register/models.py
+++ register/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Integrante(models.Model):
@@ -9,13 +10,15 @@ class Integrante(models.Model):
         return self.nome
 
 class ObjectiveKeyResult(models.Model):
+    text = RegexValidator(r'^[0-9a-zA-Z]*$', 'Só é permitido números e letras.')
+
     integrante = models.ForeignKey(Integrante, related_name='okrs', on_delete=models.CASCADE)
-    objetivo = models.CharField(max_length=200)
-    resultado_1 = models.CharField(max_length=300, null=False)
-    resultado_2 = models.CharField(max_length=300, null=False, blank=True)
-    resultado_3 = models.CharField(max_length=300, null=False, blank=True)
-    resultado_4 = models.CharField(max_length=300, null=False, blank=True)
-    resultado_5 = models.CharField(max_length=300, null=False, blank=True)
+    objetivo = models.CharField(max_length=200, validators=[text])
+    resultado_1 = models.CharField(max_length=300, null=False, validators=[text])
+    resultado_2 = models.CharField(max_length=300, null=False, blank=True, validators=[text])
+    resultado_3 = models.CharField(max_length=300, null=False, blank=True, validators=[text])
+    resultado_4 = models.CharField(max_length=300, null=False, blank=True, validators=[text])
+    resultado_5 = models.CharField(max_length=300, null=False, blank=True, validators=[text])
 
     def __str__(self):
         return self.objetivo
